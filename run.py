8b5034ef2c58d48d530df38086437e1dfa1439fa
DJ Uno <unodeej@gmail.com>
2020-04-05 15:55:47 -0700

    a

--- run.py
+++ run.py
@@ -5,11 +5,10 @@ import os, sys
 if (os.environ.get("DEBUG", False) == "True"):
     app.config["DEBUG"] = True
 
-    port = int(os.environ.get("PORT", 5000))
-    print >> sys.stderr, "DEBUG Mode! Port: " + str(port) 
+    port = int(os.environ.get("PORT", 80))
+    print >> sys.stderr, "DEBUG Mode! Port: " + str(port)
     app.run(host="0.0.0.0", port=port, debug=True)
 
     #app.run(debug=True)
 else:
     app.run()
-
