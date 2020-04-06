4a31317f6103b912275aff58455eeb5a9ef75c05
DJ Uno <unodeej@gmail.com>
2020-04-05 20:32:28 -0700

    a

--- run.py
+++ run.py
@@ -5,14 +5,14 @@ import os, sys
 if (os.environ.get("DEBUG", False) == "True"):
     app.config["DEBUG"] = True
 
-    port = int(os.environ.get("PORT", 80))
+    # port = int(os.environ.get("PORT", 80))
     # port = process.env.PORT || 80
     # const PORT = process.env.PORT || 5001;
 
     #app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
 
-    print >> sys.stderr, "DEBUG Mode! Port: " + str(port)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    print >> sys.stderr, "DEBUG Mode! Port: " + str(process.env.port)
+    app.run(host="0.0.0.0", port=process.env.port, debug=True)
 
     #app.run(debug=True)
 else:
